011cc1c32f09d4fcfeb46038ae3069df0c5a48a9
bluefrog1413 <162390173+bluefrog1413@users.noreply.github.com>
2025-10-26 23:27:14 +0900

    feat(Cars): 자동차 이름 예외처리 구현

--- src/main/java/racingcar/Cars.java
+++ src/main/java/racingcar/Cars.java
@@ -13,6 +13,26 @@ public class Cars {
         this.carName = name.split(separator);
         this.position = new Integer[carName.length];
         Arrays.fill(position, 0);
+
+        exceptionCarName();
+    }
+
+    public void exceptionCarName(){
+        for(String name : carName){
+            if(name.length() > 5) {
+                throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+            }
+
+            if(!IsDigit(name)) throw new IllegalArgumentException("자동차 이름은 문자만 가능합니다.");
+        }
+    }
+
+    public boolean IsDigit(String name){
+        boolean isChar = true;
+        for(int i = 0; i < name.length(); i++){
+            if(Character.isDigit(name.charAt(i))) isChar = false;
+        }
+        return isChar;
     }
 
     public void finalWinner(){
